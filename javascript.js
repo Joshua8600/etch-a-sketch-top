80a053955231fcdb2bcbff547ad23624efd94543
Joshua8600 <105813016+Joshua8600@users.noreply.github.com>
2026-06-09 15:59:06 -0700

    (WIP) Create Button and Function to Make New Grid

--- javascript.js
+++ javascript.js
@@ -1,24 +1,49 @@
 let gridContainer = document.querySelector(".grid-container");
-let gridSquares
 
 function createGrid() {
     for (let i = 1; i <= 256; i++) {
         let square = document.createElement("div");
         gridContainer.appendChild(square);
         square.classList.add("grid-square");
-        // square.style.width = "25px";
-        // square.style.height = "25px";
-        // square.style.borderStyle = "solid";
-        // square.style.borderColor = "black";
     }
 }
 
 createGrid();
 
-gridSquares = document.querySelectorAll(".grid-square");
+let gridSquares = document.querySelectorAll(".grid-square");
 
 gridSquares.forEach((square) => {
     square.addEventListener("mouseenter", () => {
         square.classList.add("moved-into");
-    })
-})
+    });
+});
+
+let newGridButton = document.querySelector(".new-grid-button");
+let newGridValueCheck;
+let newGridValue;
+
+newGridButton.addEventListener("click", makeNewGrid);
+
+function newGridValuePrompt() {
+    newGridValueEntry = prompt("Enter Size of Grid");
+    newGridValueCheck = +newGridValueEntry
+    return newGridValueCheck;
+}
+
+function makeNewGrid() {
+    newGridValuePrompt();
+    let sqrt = Math.sqrt(newGridValueCheck);
+    if (newGridValueCheck >= 100) {
+        newGridValue = 100;
+    } else if (Number.isInteger(sqrt)) {
+        if (newGridValueCheck < 4 ) {
+            alert("Please enter a larger number.")
+            makeNewGrid();
+        } else {
+            newGridValue = newGridValueCheck;
+        }
+    } else {
+        alert("Please enter a value that is a perfect quare.");
+        makeNewGrid();
+    }
+}
