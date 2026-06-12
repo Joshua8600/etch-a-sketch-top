22ace9e97db7eb3596385f8f7960739dd90d4d11
Joshua8600 <105813016+Joshua8600@users.noreply.github.com>
2026-06-11 21:53:43 -0700

    WIP: "New Grid" Button Mostly Functional
        The function to make a new grid had to be re-written since the last
    commit because I had misread the instructions on the project's lesson
    page.

--- javascript.js
+++ javascript.js
@@ -1,22 +1,25 @@
 let gridContainer = document.querySelector(".grid-container");
+let gridSquares
 
-function createGrid() {
+function defaultGrid() {
     for (let i = 1; i <= 256; i++) {
         let square = document.createElement("div");
         gridContainer.appendChild(square);
         square.classList.add("grid-square");
+        gridSquares = document.querySelectorAll(".grid-square");
+        sketch();
     }
 }
 
-createGrid();
+defaultGrid();
 
-let gridSquares = document.querySelectorAll(".grid-square");
-
-gridSquares.forEach((square) => {
-    square.addEventListener("mouseenter", () => {
-        square.classList.add("moved-into");
+function sketch() {
+    gridSquares.forEach((square) => {
+        square.addEventListener("mouseenter", () => {
+            square.classList.add("moved-into");
+        });
     });
-});
+}
 
 let newGridButton = document.querySelector(".new-grid-button");
 let newGridValueCheck;
@@ -25,25 +28,46 @@ let newGridValue;
 newGridButton.addEventListener("click", makeNewGrid);
 
 function newGridValuePrompt() {
-    newGridValueEntry = prompt("Enter Size of Grid");
+    newGridValueEntry = prompt("Enter Size of Grid", 1);
     newGridValueCheck = +newGridValueEntry
     return newGridValueCheck;
 }
 
 function makeNewGrid() {
     newGridValuePrompt();
-    let sqrt = Math.sqrt(newGridValueCheck);
     if (newGridValueCheck >= 100) {
         newGridValue = 100;
-    } else if (Number.isInteger(sqrt)) {
-        if (newGridValueCheck < 4 ) {
-            alert("Please enter a larger number.")
-            makeNewGrid();
-        } else {
-            newGridValue = newGridValueCheck;
-        }
     } else {
-        alert("Please enter a value that is a perfect quare.");
-        makeNewGrid();
+        newGridValue = newGridValueCheck;
+    }
+    deleteSquares();
+    createGrid();
+    sketch();
+    setGridContainerSize();
+
+}
+
+function deleteSquares() {
+    gridSquares.forEach((node) => {
+        node.remove();
+    })
+}
+
+function createGrid() {
+    for (let i = 1; i <= (newGridValue ** 2); i++) {
+        let square = document.createElement("div");
+        gridContainer.appendChild(square);
+        square.classList.add("grid-square");
+        gridSquares = document.querySelectorAll(".grid-square");
+    }
+}
+
+function setGridContainerSize() {
+    if (newGridValue >= 16) {
+        gridContainer.style.Width = "400px";
+        gridContainer.style.Height = "400px";
+    } else {
+        gridContainer.style.width = `${newGridValue * 25}px`;
+        gridContainer.style.height = `${newGridValue * 25}px`;
     }
 }
